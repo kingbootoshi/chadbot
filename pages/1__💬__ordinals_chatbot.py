828b6b755889904ce14b97b7e76ccd956b5cba8c
Bootoshi <127834715+kingbootoshi@users.noreply.github.com>
2023-12-29 23:01:14 -0800

    Update 1__💬__ordinals_chatbot.py

--- pages/1__💬__ordinals_chatbot.py
+++ pages/1__💬__ordinals_chatbot.py
@@ -24,7 +24,7 @@ from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 import pysqlite3
 import sys
 
-sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 #STREAMLIT START
 st.set_page_config(
